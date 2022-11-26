d97715816cb26ca3e9831998802c540e73117ec9
SamoKopecky <skopecky@redhat.com>
2022-11-26 18:17:47 +0100

    feat: Add dil process
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- text/code/dilithium.go
+++ text/code/dilithium.go
@@ -1,185 +1,132 @@
 package dilithium#
 
-import (
-	"crypto/rand"
-	"encoding/binary"
-	"math"
+func KeyGen() (pk, sk []byte) {
+	zeta := shake256(genRand(256), 128)
+	rho := zeta[:32]
+	rho_dash := zeta[32:96]
+	K := zeta[96:]
 
-	"golang.org/x/crypto/sha3"
-)
-
-func modP(a, mod int) (o int) {
-	o = ((a % mod) + mod) % mod
-	return
-}
-
-func modPM(a, mod int) int {
-	first := int(math.Abs(float64(((a % mod) - mod) % mod)))
-	if first <= mod/2 {
-		return int(-first)
+	A_hat := expandA(rho)
+	s_1, s_2 := expandS(rho_dash)
+	for i := 0; i < L; i++ {
+		t[i] = mulPolyVec(A_hat[i], nttPolyVec(s_1))
 	}
-	return int(modP(a, mod))
+	t = addPolyVec(invNttPolyVec(t), s_2)
+	t_1, t_0 := powerToRoundPolyVec(t, D)
+
+	pk = append(rho, bitPack(t_1, 10))
+	sk = append(rho, K)
+	sk = append(sk, shake256(pk, 32))
+	sk = append(sk, bitPack(s_1, 3))
+	sk = append(sk, bitPack(s_2, 3))
+	sk = append(sk, bitPack(t_0, 13))
+	return
 }
 
-func powerToRound(r, d int) (int, int) {
-	r = modP(r, Q)
-	r0 := modPM(r, 1<<D)
-	return (r - r0) / (1 << D), r0
-}
+func Sign(sk []byte, message []byte) (sigma []byte) {
+	rho := sk[:32]
+	K := sk[32:64]
+	tr := sk[64:96]
+	s_1 := bitUnpack(sk[96:SBytes+96], 3)
+	s_2 := bitUnpack(sk[96+SBytes:SBytes*2+96], 3)
+	t_0 := bitUnpack(sk[96+SBytes*2:], 13)
+	A_hat := expandA(rho)
+	s_1_hat := nttPolyVec(s_1)
+	s_2_hat := nttPolyVec(s_2)
+	t_0_hat := nttPolyVec(t_0)
 
-func decompose(r, alpha int) (r_1, r_0 int) {
-	r = modP(r, Q)
-	r_0 = modPM(r, alpha)
-	if r-r_0 == Q-1 {
-		r_1 = 0
-		r_0 -= 1
-	} else {
-		r_1 = int((r - r_0) / alpha)
+	shake := append(tr, message...)
+	mi := shake256(shake, 64)
+	shake = append(K, mi...)
+	rho_dash := shake256(shake, 64)
+
+	kappa := -L
+
+	for true {
+	kappa += L
+
+	y := expandMask(rho_dash, kappa)
+	y_hat := nttPolyVec(y)
+	for i := 0; i < L; i++ {
+		w[i] = invNtt(mulPolyVec(A_hat[i], y_hat))
+	}
+	w_1 := highBitsPolyVec(w, 2*GammaTwo)
+
+	shake = append(mi, bitPack(w_1, 6))
+	c_wave := shake256(shake, 32)
+	c := sampleInBall(c_wave)
+
+	cs_2 := invNttPolyVec(scalePolyVecByPoly(s_2_hat, c_hat))
+	cs_1 := invNttPolyVec(scalePolyVecByPoly(s_1_hat, c_hat))
+	z := addPolyVec(y, cs_1),
+
+	w_sub_cs_2 := subPolyVec(w, cs_2)
+	r_0 := lowBitsPolyVec(w_sub_cs_2, 2*GammaTwo)
+	if checkNormPolyVec(z, GammaOne-Beta) || checkNormPolyVec(r_0, GammaTwo-Beta) {
+		continue
+	}
+
+	ct_0 := invNttPolyVec(scalePolyVecByPoly(t_0_hat, c_hat))
+	ct_0_inv := inversePolyVec(ct_0)
+	added_vecs := addPolyVec(w_sub_cs_2, ct_0)
+	h := makeHintPolyVec(ct_0_inv, added_vecs, GammaTwo*2)
+
+	ones := 0
+	for i := 0; i < len(h); i++ {
+		for j := 0; j < N; j++ {
+			if h[i][j] == 1 {
+				ones++
+			}
+		}
+	}
+	if checkNormPolyVec(modPMPolyVec(ct_0, Q), GammaTwo) {
+		continue
+	}
+	if ones > Omega {
+		continue
+	}
+
+	z_packed := bitPackAlteredPolyVec(z, GammaOne, 18)
+	h_packed := bitPackHint(h)
+	sigma = append(c_wave, z_packed...)
+	sigma = append(sigma, h_packed...)
+	break
 	}
 	return
 }
 
-func highBits(r, alpha int) (r_1 int) {
-	r_1, _ = decompose(r, alpha)
-	return
-}
+func Verify(pk, message, sigma []byte) (verified bool) {
+	rho := pk[:32]
+	t_1_bytes := pk[32:]
+	c_wave := sigma[:32]
+	z := bitUnpackAlteredPolyVec(sigma[32:ZBytes+32], GammaOne, 18)
+	h := bitUnpackHint(sigma[ZBytes+32:])
+	t_1 := bitUnpackPolyVec(t_1_bytes, 10)
 
-func lowBits(r, alpha int) (r_0 int) {
-	_, r_0 = decompose(r, alpha)
+	A_hat := expandA(rho)
+
+	shake := append(rho, t_1_bytes...)
+	shake = append(shake256(shake, 32), message...)
+	mi := shake256(shake, 64)
+
+	c := sampleInBall(c_wave)
+	c_hat := ntt(c)
+
+	z_hat := nttPolyVec(z)
+
+	for i := 0; i < L; i++ {
+		Az[i] = mulPolyVec(A_hat[i], z_hat)
+	}
+	t_1_scaled_hat := nttPolyVec(scalePolyVecByInt(t_1, 1<<D))
+	ct_1 := scalePolyVecByPoly(t_1_scaled_hat, c_hat)
+
+	r := invNttPolyVec(subPolyVec(Az, ct_1))
+	w_1 := useHintPolyVec(h, r, 2*GammaTwo)
+	shake = append(mi, bitUnpack(w_1, 6))
+	verified = BytesEqual(c, shake256(shake, 32))
 	return
 }
 
 func makeHint(z, r, alpha int) bool {
 	return highBits(r, alpha) != highBits(r+z, alpha)
-}
-
-func useHint(h bool, r, alpha int) int {
-	m := int((Q - 1) / alpha)
-	r1, r0 := decompose(r, alpha)
-	if h && r0 > 0 {
-		return modP(r1+1, m)
-	} else if h && r0 <= 0 {
-		return modP(r1-1, m)
-	}
-	return r1
-}
-
-func sampleInBall(c_wave []byte) (c []int) {
-	c = make([]int, 256)
-	shake := sha3.NewShake256()
-	shake.Write(c_wave)
-	o := make([]byte, 8)
-	shake.Read(o)
-
-	bits := bytesToBits(o)[:Tau]
-	for i := 256 - Tau; i < 256; i++ {
-		j_byte := make([]byte, 1)
-		j := byte(255)
-		for j > byte(i) {
-			shake.Read(j_byte)
-			j = j_byte[0]
-		}
-		s := bits[i-(256-Tau)]
-		c[i] = c[j]
-		c[j] = int(1 - int8(2*s))
-	}
-	return
-}
-
-func bytesToBits(bytes []byte) (bits []byte) {
-	for i := 0; i < len(bytes); i++ {
-		for j := 0; j < 8; j++ {
-			bits = append(bits, extractBit(int(bytes[i]), j))
-		}
-	}
-	return
-}
-
-func polyToBits(poly []int, l int) (bits []byte) {
-	for i := 0; i < 256; i++ {
-		for j := 0; j < l; j++ {
-			bits = append(bits, extractBit((poly[i]), j))
-		}
-	}
-	return
-}
-
-func extractBit(from int, power int) (bit byte) {
-	bit = byte(from & (1 << power) >> power)
-	return
-}
-
-func genRand(bits int) (xofOutput []byte) {
-	len := bits / 8
-	xofOutput = make([]byte, len)
-	randBytes := make([]byte, 256)
-	rand.Read(randBytes)
-	xofOutput = shake256(randBytes, len)
-	return
-}
-
-func expandS(ro_dash []byte) (vectors [][]int) {
-	for i := 0; i < L+K; i++ {
-		poly := []int{}
-		shake := sha3.NewShake256()
-		i_bytes := make([]byte, 2)
-		i_bytes[0] = byte(i)
-		i_bytes[1] = byte(i >> 8)
-		shake.Write(ro_dash)
-		shake.Write(i_bytes)
-
-		for len(poly) < N {
-			o := make([]byte, 1)
-			shake.Read(o)
-
-			two_ints := [...]uint8{uint8(o[0]) >> 4, uint8(o[0]) & 0xF}
-
-			if Eta == 2 {
-				for _, v := range two_ints {
-					if v >= 15 {
-						continue
-					}
-					if v < 0 {
-						continue
-					}
-					poly = append(poly, Eta-(int(v)%5))
-				}
-			}
-			// TODO: eta = 4
-		}
-		vectors = append(vectors, poly[:N])
-	}
-	return
-}
-
-func expandMask(ro_dash []byte, kappa int) (y [][]int) {
-	for i := 0; i < L; i++ {
-		poly := make([]int, N)
-		shake := sha3.NewShake256()
-		shake.Write(ro_dash)
-		sum := kappa + i
-		bytes := make([]byte, 2)
-		bytes[0] = byte(sum)
-		bytes[1] = byte(sum >> 8)
-		shake.Write(bytes)
-
-		for j := 0; j < N; j++ {
-			o := make([]byte, 4)
-			shake.Read(o)
-			o[0] = 0
-			o[1] = o[1] & 2
-			poly[j] = GammaOne - int(binary.BigEndian.Uint32(o))
-		}
-		y = append(y, poly)
-	}
-	return
-}
-
-func BytesEqual(a, b []byte) (equal bool) {
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
 }
